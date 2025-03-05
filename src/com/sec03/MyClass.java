93a5da31aeac812a7b732e459a59c9886b972869
rktclgh <songchih@icloud.com>
2025-03-05 14:52:58 +0900

    3/5

---
+++ src/com/sec03/MyClass.java
@@ -0,0 +1,12 @@
+package com.sec03;
+
+public class MyClass {
+
+
+    public static void main(String[] args) {
+        System.out.println(new MyClass());
+
+        MyClass m = new MyClass();
+
+    }
+}
