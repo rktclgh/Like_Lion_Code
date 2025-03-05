5c97e9d436d3c79d2d0cbe93eecadaf3d201c7a9
rktclgh <songchih@icloud.com>
2025-03-05 16:22:54 +0900

    3/5finish

--- src/com/sec03/MTest02.java
+++ src/com/sec03/MTest02.java
@@ -1,6 +1,9 @@
 package com.sec03;
 
+import java.util.Random;
+
 public class MTest02 {
+
 
     public static void main(String[] args) {
 //        Integer i = new Integer(100);
@@ -19,6 +22,19 @@ public class MTest02 {
 
         String res =str.toUpperCase();
         System.out.println(" 출력: "+res +" -> 소문자로: "+ res.toLowerCase());
+
+        //java.util.Random 클래스가 있다. 빈 객체를 생성하고 0~100까지 난수 생성하는 메소드 호출해서 출력 해보자
+        //1. 생성자 확인 -> 2. nextInt(int)를 활용해 보자.
+        Random r = new Random();
+        int rand = r.nextInt( 101);
+        System.out.println(rand);
+
+        Random r2 = new Random();
+        System.out.println(r2.nextInt(101));
+
+        int r3 = (int)(Math.random()*101);
+        System.out.println(r3);
+
 
     }
 }
